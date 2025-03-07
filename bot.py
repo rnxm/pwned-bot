02a7d0e3782f6ef2f65ecccb91ee415e1c4f31fa
rnxm <63553775+rnxm@users.noreply.github.com>
2025-03-07 13:08:48 +0100

    updated messages

--- bot.py
+++ bot.py
@@ -21,13 +21,13 @@ async def leaked(interaction: discord.Interaction, email: str = None, name: str 
         return await interaction.response.send_message("Meg kell adnod vagy az e-mailed, vagy a nevedet!", ephemeral=True)
     if email:
         if email in [line.split(";")[0] for line in leaked]:
-            return await interaction.response.send_message("Az adataid benne vannak a kiszivárogtatott adatbázisban.", ephemeral=True)
+            return await interaction.response.send_message("# Az adataid benne vannak a kiszivárogtatott adatbázisban.\n**Mi a teendő?**\nAmennyiben úgy érzed, az adataidat jogtalanul kezelte a reFilc fejlesztője, jelentsd be az adatvédelmi incidenst a Nemzeti Adatvédelmi és Információszabadsági Hivatalnak ([NAIH](https://www.naih.hu/)).", ephemeral=True)
         else:
-            return await interaction.response.send_message("Az adataid nincsenek benne a kiszivárogtatott adatbázisban.", ephemeral=True)
+            return await interaction.response.send_message("# Az adataid nincsenek benne a kiszivárogtatott adatbázisban.", ephemeral=True)
     if name:
         if email in [line.split(";")[1] for line in leaked]:
-            return await interaction.response.send_message("Az adataid benne vannak a kiszivárogtatott adatbázisban.", ephemeral=True)
+            return await interaction.response.send_message("# Az adataid benne vannak a kiszivárogtatott adatbázisban.\n**Mi a teendő?**\nAmennyiben úgy érzed, az adataidat jogtalanul kezelte a reFilc fejlesztője, jelentsd be az adatvédelmi incidenst a Nemzeti Adatvédelmi és Információszabadsági Hivatalnak ([NAIH](https://www.naih.hu/)).", ephemeral=True)
         else:
-            return await interaction.response.send_message("Az adataid nincsenek benne a kiszivárogtatott adatbázisban. Név alapján megnézni nem olyan biztos, hogy helyesen találja meg az adatot, így ajánlatos emailt használni.", ephemeral=True)
+            return await interaction.response.send_message("# Az adataid nincsenek benne a kiszivárogtatott adatbázisban. Név alapján megnézni nem olyan biztos, hogy helyesen találja meg az adatot, így ajánlatos emailt használni.", ephemeral=True)
 
 bot.run(os.getenv("TOKEN"))
